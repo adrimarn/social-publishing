f0a08f0edbd92e7f16e358ba160bfc686c8a2294
Adrien MARION <adrien.marion@epitech.eu>
2023-02-17 11:20:41 +0100

    Optimize and adding JSDoc

--- routes/tiktok.js
+++ routes/tiktok.js
@@ -8,109 +8,102 @@ const {
   TIKTOK_CLIENT_KEY: CLIENT_KEY,
   TIKTOK_CLIENT_SECRET: CLIENT_SECRET,
   TIKTOK_REDIRECT_URI: REDIRECT_URI,
+  TIKTOK_SCOPES: SCOPES,
 } = process.env;
 
-const SCOPES = process.env.TIKTOK_SCOPES.split(",");
-const STRINGIFIED_SCOPES = SCOPES.join("%2c");
+const STRINGIFIED_SCOPES = SCOPES.split(",").join("%2c");
 
+/**
+ * @route GET /tiktok
+ * @description Redirects user to the home page.
+ */
 router.get("/", (req, res) => {
   res.redirect("/");
 });
 
-// OAuth TikTok
+/**
+ * @route GET /tiktok/login
+ * @description Redirects user to TikTok login page with the provided parameters.
+ */
 router.get("/login", (req, res) => {
   const url = `https://open-api.tiktok.com/platform/oauth/connect/?client_key=${CLIENT_KEY}&response_type=code&scope=${STRINGIFIED_SCOPES}&redirect_uri=${REDIRECT_URI}`;
   res.redirect(url);
 });
 
+/**
+ * @route GET /tiktok/callback
+ * @description Callback endpoint for TikTok authentication. Sets session variables for access_token and open_id.
+ * @async
+ */
 router.get("/callback", async (req, res) => {
-  const { code, state } = req.query;
+  const { code } = req.query;
   const uri = `https://open-api.tiktok.com/oauth/access_token/?client_key=${CLIENT_KEY}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code`;
-  const response = await fetch(uri);
-  const data = await response.json();
-  const {
-    data: { access_token, open_id },
-  } = data;
+  const { data } = await axios.get(uri);
+  const { access_token, open_id } = data.data;
   req.session.access_token = access_token;
   req.session.open_id = open_id;
-  console.log("data", data);
-  console.log("access_token", access_token);
-  res.redirect("/tiktok/publish");
+  res.redirect("/tiktok/upload");
 });
 
-router.get("/publish", async (req, res) => {
+/**
+ * @route GET /tiktok/upload
+ * @description Render the TikTok uploading page with user information.
+ */
+router.get("/upload", async (req, res) => {
   const { access_token } = req.session;
   if (!access_token) {
     res.redirect("/");
     return;
   }
   const uri = `https://open.tiktokapis.com/v2/user/info/?fields=display_name,open_id,union_id,avatar_url`;
-  const data = await fetchWithToken(uri, access_token);
-  console.log("data", data);
-  // get data.data.user
-
-  const {
-    data: { user },
-  } = data;
-  console.log("info user:", user);
+  const { data } = await axios.get(uri, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  const { user } = data.data;
+  req.session.user = user;
   res.render("tiktok", { user });
 });
 
-router.post("/publish", async (req, res) => {
-  const { access_token, open_id } = req.session;
-  if (!access_token) {
-    res.redirect("/");
-    return;
-  }
-  const { videoUrl } = req.body;
-  console.log("videoUrl", videoUrl);
-  console.log("access_token", access_token);
-  console.log("open_id", open_id);
-  const uri = `https://open-api.tiktok.com/share/video/upload/?access_token=${access_token}&open_id=${open_id}`;
-
-  // Download video URL and save to local file
-  //const videoFile = await fetch(videoUrl);
-  //const video = await videoFile.arrayBuffer();
-  //await fs.promises.writeFile("temp.mp4", Buffer.from(video));
-
-  console.log("videoUrl", videoUrl);
-  const videoStream = await axios({
-    method: "GET",
-    responseType: "stream",
-    url: videoUrl,
-  });
-
-  console.log("videoStream", videoStream.data)
-
-  // Create form data with video file
-  const form = new FormData();
-  form.append("video", videoStream.data);
-
-  // Upload video to TikTok using axios
-  const data = await axios({
-    method: "POST",
-    url: uri,
-    data: form,
-    headers: {
-      'Content-Type': 'multipart/form-data',
-        ...form.getHeaders(),
+/**
+ * @route POST /tiktok/upload
+ * @description Uploads a video to TikTok.
+ */
+router.post("/upload", async (req, res) => {
+  try {
+    const { access_token, open_id } = req.session;
+    if (!access_token) {
+      res.redirect("/");
+      return;
     }
-  });
-
-  //console.log("data", data.data);
-
-  res.redirect("/tiktok/publish");
+    const { videoUrl } = req.body;
+    const videoStream = await axios({
+      method: "GET",
+      responseType: "stream",
+      url: videoUrl,
+    });
+    const form = new FormData();
+    form.append("video", videoStream.data);
+    const uri = `https://open-api.tiktok.com/share/video/upload/?access_token=${access_token}&open_id=${open_id}`;
+    const { data } = await axios.post(uri, form, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        ...form.getHeaders(),
+      },
+    });
+    res.render("tiktok", {
+      user: req.session.user,
+      success: `Video uploaded successfully`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("tiktok", {
+      user: req.session.user,
+      error: `Failed to upload video`,
+    });
+  }
 });
-
-const fetchWithToken = async (uri, token) => {
-  const response = await fetch(uri, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-  });
-  return await response.json();
-};
 
 module.exports = router;
---
+++ utils/instagram.js
@@ -0,0 +1,98 @@
+const { FB_API_VERSION: API_VERSION } = process.env;
+
+/**
+ * Creates a container for the video in the specified Instagram account
+ *
+ * @param {string} igUserId - The Instagram user ID of the account to publish the video to
+ * @param {string} videoUrl - The URL of the video to be published
+ * @param {string} access_token - The access token for the Facebook API
+ * @return {Promise<string>} - Promise that resolves to the ID of the media container
+ */
+const createMediaContainer = async (igUserId, videoUrl, access_token) => {
+  const igMediaUri = `https://graph.facebook.com/${API_VERSION}/${igUserId}/media?media_type=REELS&video_url=${videoUrl}&access_token=${access_token}`;
+  const igMediaData = await fetch(igMediaUri, { method: "POST" });
+  const { id: igMediaContainerId } = await igMediaData.json();
+  return igMediaContainerId;
+};
+
+/**
+ * Checks the upload status of the media container until it's finished or an error occurs
+ *
+ * @param {string} igMediaContainerId - The ID of the media container to check
+ * @param {string} access_token - The access token for the Facebook API
+ * @return {Promise<void>} - Promise that resolves once the upload status is finished or an error occurs
+ * @throws {Error} - Throws an error if the upload status is not finished after the maximum number of attempts
+ */
+const checkUploadStatus = async (igMediaContainerId, access_token) => {
+  const MAX_ATTEMPTS = 30;
+  const DELAY_MS = 1000;
+
+  for (let attempt = 0; attempt < MAX_ATTEMPTS; attempt++) {
+    const igMediaContainerUri = `https://graph.facebook.com/${API_VERSION}/${igMediaContainerId}?fields=status_code&access_token=${access_token}`;
+    const igMediaContainerData = await fetch(igMediaContainerUri);
+    const igMediaContainer = await igMediaContainerData.json();
+    const { status_code, error } = igMediaContainer;
+
+    if (error || status_code === "ERROR") {
+      throw new Error(error?.message ?? "Unknown error");
+    }
+
+    if (status_code === "FINISHED") {
+      return;
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, DELAY_MS));
+  }
+
+  throw new Error("Reached maximum number of attempts without success");
+};
+
+/**
+ * Retrieves the Instagram business accounts linked to the user's Facebook account.
+ *
+ * @async
+ * @param {string} access_token - The access token to use to authenticate the request to Facebook's Graph API.
+ * @returns {Promise<Array>} An array of objects containing information about the Instagram business accounts.
+ * @throws {Error} If an error occurs while retrieving the data from Facebook's Graph API.
+ */
+async function getAccountsFromFacebookApi(access_token) {
+  const uri = `https://graph.facebook.com/${API_VERSION}/me/accounts?fields=instagram_business_account,name&access_token=${access_token}`;
+  const accountResponse = await fetch(uri);
+  const { data: accountData, error } = await accountResponse.json();
+  if (error) {
+    throw new Error(error.message);
+  }
+  return accountData;
+}
+
+/**
+ * Retrieves the Instagram username associated with the specified Instagram business account.
+ *
+ * @async
+ * @param {string} access_token - The access token to use to authenticate the request to Facebook's Graph API.
+ * @param {string} accountId - The ID of the Instagram business account.
+ * @returns {Promise<string>} The Instagram username associated with the account.
+ */
+async function getInstagramUsernameForAccount(access_token, accountId) {
+  const igUsernameUri = `https://graph.facebook.com/${API_VERSION}/${accountId}?fields=username&access_token=${access_token}`;
+  const usernameResponse = await fetch(igUsernameUri);
+  const { username } = await usernameResponse.json();
+  return username;
+}
+
+const getInstagramUserId = async (pageId, access_token) => {
+  const igUserUri = `https://graph.facebook.com/${API_VERSION}/${pageId}?fields=instagram_business_account&access_token=${access_token}`;
+  const igUserData = await fetch(igUserUri);
+  const {
+    instagram_business_account: { id },
+  } = await igUserData.json();
+  return id;
+};
+
+module.exports = {
+    createMediaContainer,
+    checkUploadStatus,
+    getAccountsFromFacebookApi,
+    getInstagramUsernameForAccount,
+    getInstagramUserId,
+};
